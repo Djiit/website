731e4dc581c8f720dd93ca4513ebf3579fd59605
Julien Tanay <julien.tanay@gmail.com>
2025-06-22 15:52:40 +0200

    fix: remove template bits

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,12 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Hello, I'm Julien
       </h1>
       <p className="mb-4">
-        Hi, I'm Julien. I'm a Dev+Ops guy currently working as a Staff SRE at Doctolib from Paris and Nantes.
-        I'm a casual speaker
+        I'm a Dev+Ops guy currently working as a Staff SRE at Doctolib from
+        Paris and Nantes. I'm a casual speaker, former manager and everyday
+        project hacker.
       </p>
       <div className="my-8">
         <BlogPosts />
